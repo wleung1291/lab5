5497b2ff19adb66a5fc6429cf6f8c6a1caadcfda
Wilson <w7leung@ucsd.edu>
2016-02-04 04:55:45 +0000

    putting it together lab

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,10 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
+exports.addFriend = function(req, res) {
 	// Your code goes here
- }
+	/*var newFriend = {	
+		'name',
+        'description',
+        'imageURL'
+      },*/
+};
